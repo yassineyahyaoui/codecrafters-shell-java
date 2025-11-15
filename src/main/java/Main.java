521680f32a125f04071295ad2e2a9ae4deca0cb3
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2025-11-15 19:16:56 +0100

    Locate executable files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,32 @@
-    import java.util.Arrays;
-    import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Scanner;
 
-    public class Main {
-        public static void main(String[] args) throws Exception {
-            while (true) {
-                System.out.print("$ ");
-                Scanner scanner = new Scanner(System.in);
-                String input = scanner.nextLine();
-                if (input.equals("exit 0")) break;
-                else if (input.split(" ")[0].equals("echo")) {
-                    System.out.println(input.substring(5));
-                } else if (input.split(" ")[0].equals("type")) {
-                    String arguments = input.substring(5);
-                    String[] allowedArguments = {"exit", "echo", "type"};
-                    if (Arrays.asList(allowedArguments).contains(arguments)) {
-                        System.out.println(arguments + " is a shell builtin");
-                    } else {
-                        System.out.println(arguments + ": not found");
-                    }
-                } else {
-                    System.out.println(input + ": command not found");
+public class Main {
+    public static void main(String[] args) throws Exception {
+        while (true) {
+            System.out.print("$ ");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (input.equals("exit 0")) break;
+            else if (input.split(" ")[0].equals("echo")) {
+                System.out.println(input.substring(5));
+            } else if (input.split(" ")[0].equals("type")) {
+                String arguments = input.substring(5);
+                String[] allowedArguments = {"exit", "echo", "type"};
+                if (Arrays.asList(allowedArguments).contains(arguments)) {
+                    System.out.println(arguments + " is a shell builtin");
                 }
+                Path path = Path.of(arguments);
+                if (Files.exists(path) && Files.isExecutable(path)) {
+                    System.out.println(arguments + " is " + path.toAbsolutePath());
+                } else {
+                    System.out.println(arguments + ": not found");
+                }
+            } else {
+                System.out.println(input + ": command not found");
             }
         }
     }
+}
