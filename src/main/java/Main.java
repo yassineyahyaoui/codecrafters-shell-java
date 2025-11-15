40c66d2a2b3016629a5cc5a5fa552d7825ad6d88
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2025-11-15 16:22:39 +0100

    Handle invalid commands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
-        System.out.print("$ ");
+        String input = new Scanner(System.in).nextLine();
+        System.out.println(input);
     }
 }
