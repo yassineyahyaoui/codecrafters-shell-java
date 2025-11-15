f819374f334775118f97a0d8f66350a11ca0c441
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2025-11-15 17:58:15 +0100

    Implement echo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@
                 Scanner scanner = new Scanner(System.in);
                 String input = scanner.nextLine();
                 if (input.equals("exit 0")) break;
-                else if (input.split("")[0].equals("echo")) {
+                else if (input.split(" ")[0].equals("echo")) {
                     System.out.println(input.substring(5));
                 } else {
                     System.out.println(input + ": command not found");
