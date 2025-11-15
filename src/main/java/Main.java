1cb2baec92badb5a3c95343e2408a0df1a1b2180
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2025-11-15 17:13:04 +0100

    Fix test failure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,14 @@
-import java.util.Scanner;
+    import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        String input = new Scanner(System.in).nextLine();
-        System.out.println(input);
+    public class Main {
+        public static void main(String[] args) throws Exception {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("$ ");
+            System.out.flush();
+
+            String command = scanner.nextLine().trim();
+            if (!command.isEmpty()) {
+                System.out.println(command + ": command not found");
+            }
+        }
     }
-}
