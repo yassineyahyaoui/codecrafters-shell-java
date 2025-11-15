434f61b0ae3893120f1d43f6530ade55cc0b7df9
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2025-11-15 17:38:18 +0100

    Implement exit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,10 @@
                 System.out.print("$ ");
                 Scanner scanner = new Scanner(System.in);
                 String input = scanner.nextLine();
-                System.out.println(input + ": command not found");
                 if (input.equals("exit 0")) {
                     break;
+                } else {
+                    System.out.println(input + ": command not found");
                 }
             }
         }
