e95e18089b95a3c95a2b804a358c32308d7ce07c
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2025-11-16 22:26:30 +0100

    Locate executable files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.File;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -9,24 +8,34 @@ public class Main {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
+
             if (input.equals("exit 0")) break;
             else if (input.split(" ")[0].equals("echo")) {
                 System.out.println(input.substring(5));
+
             } else if (input.split(" ")[0].equals("type")) {
                 String arguments = input.substring(5);
                 String[] allowedArguments = {"exit", "echo", "type"};
+
                 if (Arrays.asList(allowedArguments).contains(arguments)) {
                     System.out.println(arguments + " is a shell builtin");
                 }
-                Path path = Path.of(arguments);
-                if (Files.exists(path) && Files.isExecutable(path)) {
-                    System.out.println(arguments + " is " + path.toAbsolutePath());
-                } else {
-                    System.out.println(arguments + ": not found");
+
+                String[] paths = System.getenv("PATH").split(File.pathSeparator);
+                for (String path : paths) {
+                    File file = new File(path, arguments);
+                    if (file.exists()) {
+                        System.out.println(arguments + " is " + file.getAbsolutePath());
+                    }
                 }
+
+                System.out.println(arguments + ": not found");
+
             } else {
                 System.out.println(input + ": command not found");
             }
+
+            scanner.close();
         }
     }
 }
