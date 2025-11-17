a292a8b068a9986e8c8b0b71895fda1272fcfb55
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2025-11-17 22:13:38 +0100

    Reformat code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,58 +11,74 @@ public class Main {
                 String input = scanner.nextLine();
                 String[] paths = System.getenv("PATH").split(File.pathSeparator);
 
-                if (input.equals("exit 0")) break;
-
-                else if (input.equals("pwd")) {
-                    System.out.println(Paths.get("").toAbsolutePath().toString());
-                }
-
-                else if (input.split(" ")[0].equals("echo")) {
-                    System.out.println(input.substring(5));
-
+                if (isExitCommand(input)) {
+                    break;
+                } else if (input.equals("pwd")) {
+                    handlePwd();
+                } else if (input.split(" ")[0].equals("echo")) {
+                    handleEcho(input);
                 } else if (input.split(" ")[0].equals("type")) {
                     String arguments = input.substring(5);
-                    String[] allowedArguments = {"exit", "echo", "type", "pwd"};
-
-                    if (Arrays.asList(allowedArguments).contains(arguments)) {
-                        System.out.println(arguments + " is a shell builtin");
-                    } else {
-                        boolean found = false;
-                        for (String path : paths) {
-                            File file = new File(path, arguments);
-                            if (file.exists() && file.canExecute()) {
-                                System.out.println(arguments + " is " + file.getAbsolutePath());
-                                found = true;
-                                break;
-                            }
-                        }
-                        if (!found) {
-                            System.out.println(arguments + ": not found");
-                        }
-                    }
-
+                    handleType(arguments, paths);
                 } else {
-                    boolean found = false;
-                    List<String> arguments = new ArrayList<>();
-                    Collections.addAll(arguments, input.split(" "));
-                    for (String path : paths) {
-                        File file = new File(path, arguments.getFirst());
-                        if (file.exists() && file.canExecute()) {
-                            found = true;
-                            ProcessBuilder pb = new ProcessBuilder(arguments);
-                            pb.redirectErrorStream(true);
-                            Process process = pb.start();
-                            process.getInputStream().transferTo(System.out);
-                            break;
-                        }
-                    }
-                    if (!found) {
-                        System.out.println(input + ": command not found");
-                    }
+                    handleExternalCommand(input, paths);
                 }
             }
         } finally {
             scanner.close();
+        }
+    }
+
+    private static boolean isExitCommand(String input) {
+        return input.equals("exit 0");
+    }
+
+    private static void handlePwd() {
+        System.out.println(Paths.get("").toAbsolutePath().toString());
+    }
+
+    private static void handleEcho(String input) {
+        System.out.println(input.substring(5));
+    }
+
+    private static void handleType(String arguments, String[] paths) {
+        String[] allowedArguments = {"exit", "echo", "type", "pwd"};
+
+        if (Arrays.asList(allowedArguments).contains(arguments)) {
+            System.out.println(arguments + " is a shell builtin");
+        } else {
+            boolean found = false;
+            for (String path : paths) {
+                File file = new File(path, arguments);
+                if (file.exists() && file.canExecute()) {
+                    System.out.println(arguments + " is " + file.getAbsolutePath());
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                System.out.println(arguments + ": not found");
+            }
+        }
+    }
+
+    private static void handleExternalCommand(String input, String[] paths) throws Exception {
+        boolean found = false;
+        List<String> arguments = new ArrayList<>();
+        Collections.addAll(arguments, input.split(" "));
+        for (String path : paths) {
+            File file = new File(path, arguments.getFirst());
+            if (file.exists() && file.canExecute()) {
+                found = true;
+                ProcessBuilder pb = new ProcessBuilder(arguments);
+                pb.redirectErrorStream(true);
+                Process process = pb.start();
+                process.getInputStream().transferTo(System.out);
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println(input + ": command not found");
         }
     }
 }
