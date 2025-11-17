d295f02551d9e3c88b40135a0460e6c0f6f27ed2
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2025-11-17 21:51:35 +0100

    close scanner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,57 +3,61 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        while (true) {
-            System.out.print("$ ");
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
-            String[] paths = System.getenv("PATH").split(File.pathSeparator);
+        Scanner scanner = new Scanner(System.in);
+        try {
+            while (true) {
+                System.out.print("$ ");
+                String input = scanner.nextLine();
+                String[] paths = System.getenv("PATH").split(File.pathSeparator);
 
-            if (input.equals("exit 0")) break;
+                if (input.equals("exit 0")) break;
 
-            else if (input.split(" ")[0].equals("echo")) {
-                System.out.println(input.substring(5));
+                else if (input.split(" ")[0].equals("echo")) {
+                    System.out.println(input.substring(5));
 
-            } else if (input.split(" ")[0].equals("type")) {
-                String arguments = input.substring(5);
-                String[] allowedArguments = {"exit", "echo", "type"};
+                } else if (input.split(" ")[0].equals("type")) {
+                    String arguments = input.substring(5);
+                    String[] allowedArguments = {"exit", "echo", "type"};
 
-                if (Arrays.asList(allowedArguments).contains(arguments)) {
-                    System.out.println(arguments + " is a shell builtin");
+                    if (Arrays.asList(allowedArguments).contains(arguments)) {
+                        System.out.println(arguments + " is a shell builtin");
+                    } else {
+                        boolean found = false;
+                        for (String path : paths) {
+                            File file = new File(path, arguments);
+                            if (file.exists() && file.canExecute()) {
+                                System.out.println(arguments + " is " + file.getAbsolutePath());
+                                found = true;
+                                break;
+                            }
+                        }
+                        if (!found) {
+                            System.out.println(arguments + ": not found");
+                        }
+                    }
+
                 } else {
                     boolean found = false;
+                    List<String> arguments = new ArrayList<>();
+                    Collections.addAll(arguments, input.split(" "));
                     for (String path : paths) {
-                        File file = new File(path, arguments);
+                        File file = new File(path, arguments.getFirst());
                         if (file.exists() && file.canExecute()) {
-                            System.out.println(arguments + " is " + file.getAbsolutePath());
                             found = true;
+                            ProcessBuilder pb = new ProcessBuilder(arguments);
+                            pb.redirectErrorStream(true);
+                            Process process = pb.start();
+                            process.getInputStream().transferTo(System.out);
                             break;
                         }
                     }
                     if (!found) {
-                        System.out.println(arguments + ": not found");
+                        System.out.println(input + ": command not found");
                     }
-                }
-
-            } else {
-                boolean found = false;
-                List<String> arguments = new ArrayList<>();
-                Collections.addAll(arguments, input.split(" "));
-                for (String path : paths) {
-                    File file = new File(path, arguments.getFirst());
-                    if (file.exists() && file.canExecute()) {
-                        found = true;
-                        ProcessBuilder pb = new ProcessBuilder(arguments);
-                        pb.redirectErrorStream(true);
-                        Process process = pb.start();
-                        process.getInputStream().transferTo(System.out);
-                        break;
-                    }
-                }
-                if (!found) {
-                    System.out.println(input + ": command not found");
                 }
             }
+        } finally {
+            scanner.close();
         }
     }
 }
